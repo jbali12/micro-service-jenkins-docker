fdb2a86952c0a74f55396885e5a411f16ab2d1e4
jbali12 <88539149+jbali12@users.noreply.github.com>
2022-04-14 13:26:57 +0200

    Delete order-service directory

--- order-service/src/main/java/com/rainbowforest/orderservice/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.rainbowforest.orderservice.service;
-
-import com.rainbowforest.orderservice.domain.Order;
-
-public interface OrderService {
-    public Order saveOrder(Order order);
-}
